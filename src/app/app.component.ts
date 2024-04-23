261c573fda3ab25a829c340ddff538032cb2a97f
Paco Fuentes <pacofuentes.work@gmail.com>
2024-04-23 13:04:57 +0200

    feat: ecommerce API http connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductComponent, NgForOf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changedTitle() {
+    this.title = 'my-app con cambio de estado';
+  }
+
+  // ejecutar al iniciar el ciclo de vida
+  ngOnInit() {
+    // aqui se usa RxJS para solucionar la peticion asíncrona de forma reactiva
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+        console.log(data);
+      });
+  }
 }
